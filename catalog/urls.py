88e80d17fbed78edcbceba653c12f1e007df55f2
our ame <you@example.com>
2026-01-19 03:33:04 +0300

    fix

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, CatalogListView, CatalogDetailView, CatalogCreateView, UpdateView, \
+from catalog.views import HomeView, ContactsView, CatalogListView, CatalogDetailView, CatalogCreateView, UpdateView, \
     CatalogUpdateView, CatalogDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/',home, name='home'),
-    path('contacts/',contacts, name='contacts'),
+    path('', HomeView.as_view(), name='home'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
     path('base/',CatalogListView.as_view(), name='base'),
     path('products/<int:pk>/', CatalogDetailView.as_view(), name='product_detail'),
     path('create/', CatalogCreateView.as_view(), name='product_create'),
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
 from itertools import product
 
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView, UpdateView,DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView,DeleteView, TemplateView
 from django.urls import reverse_lazy
 from catalog.models import Product
 
@@ -26,10 +26,44 @@ class CatalogDeleteView(DeleteView):
     success_url = reverse_lazy('catalog:base')
 
 
-def home(request):
-    return render(request, 'home.html')
+class HomeView(TemplateView):
+    """Главная страница"""
+    template_name = 'catalog/home.html'
 
-def contacts(request):
-    return render(request, 'contacts.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        # Можно добавить дополнительные данные, например, последние продукты
+        context['latest_products'] = Product.objects.all().order_by('-created_at')[:3]
+        return context
 
+
+class ContactsView(TemplateView):
+    """Страница контактов"""
+    template_name = 'catalog/contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Контакты'
+
+        # Пример данных контактов (можно вынести в настройки или БД)
+        context['contacts'] = [
+            {'name': 'Телефон', 'value': '+7 (999) 123-45-67', 'icon': 'telephone'},
+            {'name': 'Email', 'value': 'info@catalog.ru', 'icon': 'envelope'},
+            {'name': 'Адрес', 'value': 'г. Москва, ул. Примерная, д. 1', 'icon': 'geo-alt'},
+            {'name': 'Режим работы', 'value': 'Пн-Пт: 9:00-18:00', 'icon': 'clock'},
+        ]
+
+        # Обработка POST запроса для формы обратной связи
+        if self.request.method == 'POST':
+            context['form_submitted'] = True
+            context['message'] = 'Спасибо! Ваше сообщение отправлено.'
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        """Обработка POST запроса (форма обратной связи)"""
+        # Здесь можно добавить логику обработки формы
+        # Например, сохранение в БД или отправку email
+        return self.get(request, *args, **kwargs)
 
